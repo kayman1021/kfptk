783fef346148ce1bf3900692abd38451f747cf46
kayman1021 <47067158+kayman1021@users.noreply.github.com>
2019-08-27 18:53:47 +0200

    v2 files added

---
+++ p00/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace p00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ImportRaw = new System.Windows.Forms.TextBox();
+            this.textBox_ExportTiff = new System.Windows.Forms.TextBox();
+            this.button_ImportRawXiaomi = new System.Windows.Forms.Button();
+            this.button_ExportTiff = new System.Windows.Forms.Button();
+            this.button_DeinterlaceVertical = new System.Windows.Forms.Button();
+            this.button_InterlaceVertical = new System.Windows.Forms.Button();
+            this.button_DeinterlaceHorizontal = new System.Windows.Forms.Button();
+            this.button_InterlaceHorizontal = new System.Windows.Forms.Button();
+            this.button_ExportIntArray = new System.Windows.Forms.Button();
+            this.button_ImportRaw14bit = new System.Windows.Forms.Button();
+            this.textBox_ExportIntArray = new System.Windows.Forms.TextBox();
+            this.button_ImportRawBrowse = new System.Windows.Forms.Button();
+            this.button_ExportTiffBrowse = new System.Windows.Forms.Button();
+            this.button_ExportIntArrayBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_ImportRaw
+            // 
+            this.textBox_ImportRaw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox_ImportRaw.Location = new System.Drawing.Point(12, 12);
+            this.textBox_ImportRaw.Multiline = true;
+            this.textBox_ImportRaw.Name = "textBox_ImportRaw";
+            this.textBox_ImportRaw.Size = new System.Drawing.Size(500, 24);
+            this.textBox_ImportRaw.TabIndex = 0;
+            // 
+            // textBox_ExportTiff
+            // 
+            this.textBox_ExportTiff.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox_ExportTiff.Location = new System.Drawing.Point(12, 150);
+            this.textBox_ExportTiff.Multiline = true;
+            this.textBox_ExportTiff.Name = "textBox_ExportTiff";
+            this.textBox_ExportTiff.Size = new System.Drawing.Size(500, 24);
+            this.textBox_ExportTiff.TabIndex = 2;
+            // 
+            // button_ImportRawXiaomi
+            // 
+            this.button_ImportRawXiaomi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ImportRawXiaomi.Location = new System.Drawing.Point(518, 42);
+            this.button_ImportRawXiaomi.Name = "button_ImportRawXiaomi";
+            this.button_ImportRawXiaomi.Size = new System.Drawing.Size(150, 24);
+            this.button_ImportRawXiaomi.TabIndex = 3;
+            this.button_ImportRawXiaomi.Text = "Import Raw Xiaomi";
+            this.button_ImportRawXiaomi.UseVisualStyleBackColor = true;
+            this.button_ImportRawXiaomi.Click += new System.EventHandler(this.button_ImportRawXiaomi_Click);
+            // 
+            // button_ExportTiff
+            // 
+            this.button_ExportTiff.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ExportTiff.Location = new System.Drawing.Point(518, 185);
+            this.button_ExportTiff.Name = "button_ExportTiff";
+            this.button_ExportTiff.Size = new System.Drawing.Size(150, 24);
+            this.button_ExportTiff.TabIndex = 4;
+            this.button_ExportTiff.Text = "Export Tiff";
+            this.button_ExportTiff.UseVisualStyleBackColor = true;
+            this.button_ExportTiff.Click += new System.EventHandler(this.button_ExportTiff_Click);
+            // 
+            // button_DeinterlaceVertical
+            // 
+            this.button_DeinterlaceVertical.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_DeinterlaceVertical.Location = new System.Drawing.Point(12, 42);
+            this.button_DeinterlaceVertical.Name = "button_DeinterlaceVertical";
+            this.button_DeinterlaceVertical.Size = new System.Drawing.Size(160, 24);
+            this.button_DeinterlaceVertical.TabIndex = 5;
+            this.button_DeinterlaceVertical.Text = "Deinterlace Vertical";
+            this.button_DeinterlaceVertical.UseVisualStyleBackColor = true;
+            this.button_DeinterlaceVertical.Click += new System.EventHandler(this.button_DeinterlaceVertical_Click);
+            // 
+            // button_InterlaceVertical
+            // 
+            this.button_InterlaceVertical.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_InterlaceVertical.Location = new System.Drawing.Point(178, 42);
+            this.button_InterlaceVertical.Name = "button_InterlaceVertical";
+            this.button_InterlaceVertical.Size = new System.Drawing.Size(160, 24);
+            this.button_InterlaceVertical.TabIndex = 6;
+            this.button_InterlaceVertical.Text = "Interlace Vertical";
+            this.button_InterlaceVertical.UseVisualStyleBackColor = true;
+            this.button_InterlaceVertical.Click += new System.EventHandler(this.button_InterlaceVertical_Click);
+            // 
+            // button_DeinterlaceHorizontal
+            // 
+            this.button_DeinterlaceHorizontal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_DeinterlaceHorizontal.Location = new System.Drawing.Point(12, 72);
+            this.button_DeinterlaceHorizontal.Name = "button_DeinterlaceHorizontal";
+            this.button_DeinterlaceHorizontal.Size = new System.Drawing.Size(160, 24);
+            this.button_DeinterlaceHorizontal.TabIndex = 7;
+            this.button_DeinterlaceHorizontal.Text = "Deinterlace Horizontal";
+            this.button_DeinterlaceHorizontal.UseVisualStyleBackColor = true;
+            this.button_DeinterlaceHorizontal.Click += new System.EventHandler(this.button_DeinterlaceHorizontal_Click);
+            // 
+            // button_InterlaceHorizontal
+            // 
+            this.button_InterlaceHorizontal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_InterlaceHorizontal.Location = new System.Drawing.Point(178, 72);
+            this.button_InterlaceHorizontal.Name = "button_InterlaceHorizontal";
+            this.button_InterlaceHorizontal.Size = new System.Drawing.Size(160, 24);
+            this.button_InterlaceHorizontal.TabIndex = 8;
+            this.button_InterlaceHorizontal.Text = "Interlace Horizontal";
+            this.button_InterlaceHorizontal.UseVisualStyleBackColor = true;
+            this.button_InterlaceHorizontal.Click += new System.EventHandler(this.button_InterlaceHorizontal_Click);
+            // 
+            // button_ExportIntArray
+            // 
+            this.button_ExportIntArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ExportIntArray.Location = new System.Drawing.Point(518, 330);
+            this.button_ExportIntArray.Name = "button_ExportIntArray";
+            this.button_ExportIntArray.Size = new System.Drawing.Size(150, 24);
+            this.button_ExportIntArray.TabIndex = 9;
+            this.button_ExportIntArray.Text = "Export Int Array";
+            this.button_ExportIntArray.UseVisualStyleBackColor = true;
+            this.button_ExportIntArray.Click += new System.EventHandler(this.button_ExportIntArray_Click);
+            // 
+            // button_ImportRaw14bit
+            // 
+            this.button_ImportRaw14bit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ImportRaw14bit.Location = new System.Drawing.Point(674, 42);
+            this.button_ImportRaw14bit.Name = "button_ImportRaw14bit";
+            this.button_ImportRaw14bit.Size = new System.Drawing.Size(150, 24);
+            this.button_ImportRaw14bit.TabIndex = 10;
+            this.button_ImportRaw14bit.Text = "Import Raw 14bit";
+            this.button_ImportRaw14bit.UseVisualStyleBackColor = true;
+            this.button_ImportRaw14bit.Click += new System.EventHandler(this.button_ImportRaw14bit_Click);
+            // 
+            // textBox_ExportIntArray
+            // 
+            this.textBox_ExportIntArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox_ExportIntArray.Location = new System.Drawing.Point(12, 300);
+            this.textBox_ExportIntArray.Multiline = true;
+            this.textBox_ExportIntArray.Name = "textBox_ExportIntArray";
+            this.textBox_ExportIntArray.Size = new System.Drawing.Size(500, 24);
+            this.textBox_ExportIntArray.TabIndex = 11;
+            // 
+            // button_ImportRawBrowse
+            // 
+            this.button_ImportRawBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ImportRawBrowse.Location = new System.Drawing.Point(518, 12);
+            this.button_ImportRawBrowse.Name = "button_ImportRawBrowse";
+            this.button_ImportRawBrowse.Size = new System.Drawing.Size(306, 24);
+            this.button_ImportRawBrowse.TabIndex = 12;
+            this.button_ImportRawBrowse.Text = "Browse for DNG";
+            this.button_ImportRawBrowse.UseVisualStyleBackColor = true;
+            this.button_ImportRawBrowse.Click += new System.EventHandler(this.button_ImportRawBrowse_Click);
+            // 
+            // button_ExportTiffBrowse
+            // 
+            this.button_ExportTiffBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ExportTiffBrowse.Location = new System.Drawing.Point(518, 150);
+            this.button_ExportTiffBrowse.Name = "button_ExportTiffBrowse";
+            this.button_ExportTiffBrowse.Size = new System.Drawing.Size(306, 24);
+            this.button_ExportTiffBrowse.TabIndex = 13;
+            this.button_ExportTiffBrowse.Text = "Browse for TIFF";
+            this.button_ExportTiffBrowse.UseVisualStyleBackColor = true;
+            this.button_ExportTiffBrowse.Click += new System.EventHandler(this.button_ExportTiffBrowse_Click);
+            // 
+            // button_ExportIntArrayBrowse
+            // 
+            this.button_ExportIntArrayBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_ExportIntArrayBrowse.Location = new System.Drawing.Point(518, 300);
+            this.button_ExportIntArrayBrowse.Name = "button_ExportIntArrayBrowse";
+            this.button_ExportIntArrayBrowse.Size = new System.Drawing.Size(306, 24);
+            this.button_ExportIntArrayBrowse.TabIndex = 14;
+            this.button_ExportIntArrayBrowse.Text = "Browse for TXT";
+            this.button_ExportIntArrayBrowse.UseVisualStyleBackColor = true;
+            this.button_ExportIntArrayBrowse.Click += new System.EventHandler(this.button_ExportIntArrayBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 369);
+            this.Controls.Add(this.button_ExportIntArrayBrowse);
+            this.Controls.Add(this.button_ExportTiffBrowse);
+            this.Controls.Add(this.button_ImportRawBrowse);
+            this.Controls.Add(this.textBox_ExportIntArray);
+            this.Controls.Add(this.button_ImportRaw14bit);
+            this.Controls.Add(this.button_ExportIntArray);
+            this.Controls.Add(this.button_InterlaceHorizontal);
+            this.Controls.Add(this.button_DeinterlaceHorizontal);
+            this.Controls.Add(this.button_InterlaceVertical);
+            this.Controls.Add(this.button_DeinterlaceVertical);
+            this.Controls.Add(this.button_ExportTiff);
+            this.Controls.Add(this.button_ImportRawXiaomi);
+            this.Controls.Add(this.textBox_ExportTiff);
+            this.Controls.Add(this.textBox_ImportRaw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ImportRaw;
+        private System.Windows.Forms.TextBox textBox_ExportTiff;
+        private System.Windows.Forms.Button button_ImportRawXiaomi;
+        private System.Windows.Forms.Button button_ExportTiff;
+        private System.Windows.Forms.Button button_DeinterlaceVertical;
+        private System.Windows.Forms.Button button_InterlaceVertical;
+        private System.Windows.Forms.Button button_DeinterlaceHorizontal;
+        private System.Windows.Forms.Button button_InterlaceHorizontal;
+        private System.Windows.Forms.Button button_ExportIntArray;
+        private System.Windows.Forms.Button button_ImportRaw14bit;
+        private System.Windows.Forms.TextBox textBox_ExportIntArray;
+        private System.Windows.Forms.Button button_ImportRawBrowse;
+        private System.Windows.Forms.Button button_ExportTiffBrowse;
+        private System.Windows.Forms.Button button_ExportIntArrayBrowse;
+    }
+}
+
---
+++ p00/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using BitMiracle.LibTiff.Classic;
+
+namespace p00
+{
+    public partial class Form1 : Form
+    {
+        RawDataTool rwt = new RawDataTool();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //GUI INTERACTION
+        private void button_ImportRawXiaomi_Click(object sender, EventArgs e)
+        {
+            rwt.ImportRawDataXiaomi(@textBox_ImportRaw.Text);
+        }
+        private void button_ImportRaw14bit_Click(object sender, EventArgs e)
+        {
+            rwt.ImportRawData14bitUncompressed(@textBox_ImportRaw.Text);
+        }
+        private void button_ExportTiff_Click(object sender, EventArgs e)
+        {
+            rwt.ExportRawDataTiff(@textBox_ExportTiff.Text);
+        }
+        private void button_ExportIntArray_Click(object sender, EventArgs e)
+        {
+            rwt.ExportRawDataArray(@textBox_ExportIntArray.Text);
+        }
+
+        private void button_ImportRawBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "DNG file|*.dng";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    textBox_ImportRaw.Text = open.FileName;
+                }
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("Failed loading file");
+            }
+        }
+        private void button_ExportIntArrayBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.Filter = "txt file|*.txt";
+                save.ShowDialog();
+                if (save.FileName != "")
+                {
+                    textBox_ExportIntArray.Text = save.FileName;
+                }
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("");
+            }
+        }
+        private void button_ExportTiffBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.Filter = "tiff file|*.tiff";
+                save.ShowDialog();
+                if (save.FileName != "")
+                {
+                    textBox_ExportTiff.Text = save.FileName;
+                }
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("");
+            }
+        }
+
+        private void button_DeinterlaceVertical_Click(object sender, EventArgs e)
+        {
+            rwt.DeinterlaceVertical();
+        }
+        private void button_DeinterlaceHorizontal_Click(object sender, EventArgs e)
+        {
+            rwt.DeinterlaceHorizontal();
+        }
+        private void button_InterlaceVertical_Click(object sender, EventArgs e)
+        {
+            rwt.InterlaceVertical();
+        }
+        private void button_InterlaceHorizontal_Click(object sender, EventArgs e)
+        {
+            rwt.InterlaceHorizontal();
+        }
+    }
+
+
+
+    class RawDataTool
+    {
+        int[,] rawData;
+        int rawWidth;
+        int rawHeight;
+
+
+
+        public void ImportRawDataXiaomi(string filename)
+        {
+            int bitsPerSample = 16;
+            using (Tiff input = Tiff.Open(@filename, "r"))
+            {
+                rawWidth = input.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
+                rawHeight = input.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
+                rawData = new int[rawWidth, rawHeight];
+            }
+            using (BinaryReader reader = new BinaryReader(File.Open(@filename, FileMode.Open)))
+            {
+                int filelength = (int)(new System.IO.FileInfo(@filename).Length);
+                int datalength = (int)(rawWidth * rawHeight * bitsPerSample / 8);
+                int start_addr = filelength - datalength;
+                int pixelcount = rawWidth * rawHeight;
+                reader.ReadBytes(start_addr);
+                Byte[] bytebuffer = reader.ReadBytes(datalength);
+                //int temp;
+                for (int i = 0; i < pixelcount; i++)
+                {
+                    rawData[i % rawWidth, i / rawWidth] = (int)(bytebuffer[2 * i]) + (int)(bytebuffer[2 * i + 1] * 256);
+                }
+            }
+            Console.WriteLine();
+        }//WORKS
+
+        public void ImportRawData14bitUncompressed(string filename)
+        {
+            using (Tiff input = Tiff.Open(@filename, "r"))
+            {
+                rawWidth = input.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
+                rawHeight = input.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
+                rawData = new int[rawWidth, rawHeight];
+            }
+
+            int bitsPerSample = 14;
+            using (BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open)))
+            {
+                int filelength = (int)(new System.IO.FileInfo(filename).Length);
+                int datalength = (int)(rawWidth * rawHeight * bitsPerSample / 8f);
+                int start_addr = filelength - datalength;
+                reader.ReadBytes(start_addr);
+                byte[] buffbuff = new byte[datalength];
+                buffbuff = reader.ReadBytes(datalength);
+                BitArray bitsbits = new BitArray(datalength * 8);
+                for (int i = 0; i < datalength; i++)
+                {
+                    int remainder = buffbuff[i];
+
+                    for (int k = 0; k < 8; k++)
+                    {
+                        int victim = remainder;
+                        int power = (int)(Math.Pow((double)2, (double)(8 - 1 - k)));
+                        int whole = victim / power;
+
+                        if (whole == 1)
+                        {
+                            bitsbits[i * 8 + k] = true;
+                        }
+                        else
+                        {
+                            bitsbits[i * 8 + k] = false;
+                        }
+                        remainder = victim - (whole * power);
+                    }
+                }
+                int[] pixelbits = new int[bitsPerSample];
+                for (int i = 0; i < rawWidth * rawHeight; i++)
+                {
+                    for (int j = 0; j < bitsPerSample; j++)
+                    {
+                        bool current = bitsbits[(i * bitsPerSample) + j];
+                        if (current)
+                        {
+                            pixelbits[j] = 1;
+                        }
+                        else
+                        {
+                            pixelbits[j] = 0;
+                        }
+                    }
+                    int summ = 0;
+                    for (int k = 0; k < bitsPerSample; k++)
+                    {
+                        summ += pixelbits[k] * (int)Math.Pow((double)2, (double)(bitsPerSample - 1 - k));
+                    }
+                }
+            }
+        }//BIG MESS
+
+        public void ExportRawDataTiff(string filename)
+        {
+            using (Tiff output = Tiff.Open(@filename, "w"))
+            {
+                output.SetField(TiffTag.IMAGEWIDTH, rawWidth);
+                output.SetField(TiffTag.IMAGELENGTH, rawHeight);
+                output.SetField(TiffTag.SAMPLESPERPIXEL, 1);
+                output.SetField(TiffTag.BITSPERSAMPLE, 16);
+                output.SetField(TiffTag.ORIENTATION, BitMiracle.LibTiff.Classic.Orientation.TOPLEFT);
+                output.SetField(TiffTag.XRESOLUTION, 72);
+                output.SetField(TiffTag.YRESOLUTION, 72);
+                output.SetField(TiffTag.RESOLUTIONUNIT, ResUnit.CENTIMETER);
+
+                output.SetField(TiffTag.PLANARCONFIG, PlanarConfig.CONTIG);
+                output.SetField(TiffTag.PHOTOMETRIC, Photometric.MINISBLACK);
+                output.SetField(TiffTag.COMPRESSION, Compression.NONE);
+                output.SetField(TiffTag.ROWSPERSTRIP, rawHeight);
+                output.SetField(TiffTag.FILLORDER, FillOrder.MSB2LSB);
+
+                byte[] bytebuffer = new byte[rawWidth * 16 / 8];
+                int rawValue;
+                for (int yyy = 0; yyy < rawHeight; yyy++)
+                {
+                    for (int xxx = 0; xxx < rawWidth; xxx++)
+                    {
+                        rawValue = rawData[xxx, yyy];
+                        bytebuffer[xxx * 2] = (byte)(rawValue % 256);
+                        bytebuffer[xxx * 2 + 1] = (byte)(rawValue / 256);
+                    }
+                    output.WriteScanline(bytebuffer, yyy);
+                }
+            }
+        }//WORKS
+
+        public void ExportRawDataArray(string filename)
+        {
+            StringBuilder sb = new StringBuilder();
+            string spacer = " ";
+            string newline = "\r\n";
+            for (int yyy = 0; yyy < rawHeight; yyy++)
+            {
+                for (int xxx = 0; xxx < rawWidth; xxx++)
+                {
+                    sb.Append(rawData[xxx, yyy] + spacer);
+                }
+                sb.Append(newline);
+            }
+            File.WriteAllText(@filename, sb.ToString());
+        }//MAY WORK
+
+        public void InterlaceVertical()
+        {
+            int[,] output = new int[rawWidth, rawHeight];
+            int halfres = rawHeight / 2;
+
+            int temp;
+
+            for (int yyy = 0; yyy < rawHeight; yyy++)
+            {
+                if (yyy < halfres)
+                {
+                    temp = 2 * yyy;
+                }
+                else
+                {
+                    temp = (2 * yyy) - rawHeight + 1;
+                }
+                for (int xxx = 0; xxx < rawWidth; xxx++)
+                {
+                    output[xxx, temp] = rawData[xxx, yyy];
+                }
+            }
+            rawData = output;
+        }//WORKS
+
+        public void InterlaceHorizontal()
+        {
+            int[,] output = new int[rawWidth, rawHeight];
+            int halfres = rawWidth / 2;
+
+            int temp;
+
+            for (int xxx = 0; xxx < rawWidth; xxx++)
+            {
+                if (xxx < halfres)
+                {
+                    temp = 2 * xxx;
+                }
+                else
+                {
+                    temp = (2 * xxx) - rawWidth + 1;
+                }
+                for (int yyy = 0; yyy < rawHeight; yyy++)
+                {
+                    output[temp, yyy] = rawData[xxx, yyy];
+                }
+            }
+            rawData = output;
+        }//WORKS
+
+        public void DeinterlaceVertical()
+        {
+            int[,] output = new int[rawWidth, rawHeight];
+            int halfres = rawHeight / 2;
+            int temp;
+
+            for (int yyy = 0; yyy < rawHeight; yyy++)
+            {
+                if (yyy % 2 == 0)
+                {
+                    temp = yyy / 2;
+                }
+                else
+                {
+                    temp = halfres + ((yyy - 1) / 2);
+                }
+
+                for (int xxx = 0; xxx < rawWidth; xxx++)
+                {
+                    output[xxx, temp] = rawData[xxx, yyy];
+                }
+            }
+            rawData = output;
+        }//WORKS
+
+        public void DeinterlaceHorizontal()
+        {
+            int[,] output = new int[rawWidth, rawHeight];
+            int halfres = rawWidth / 2;
+            int temp;
+
+            for (int xxx = 0; xxx < rawWidth; xxx++)
+            {
+                if (xxx % 2 == 0)
+                {
+                    temp = xxx / 2;
+                }
+                else
+                {
+                    temp = halfres + ((xxx - 1) / 2);
+                }
+
+                for (int yyy = 0; yyy < rawHeight; yyy++)
+                {
+                    output[temp, yyy] = rawData[xxx, yyy];
+                }
+            }
+            rawData = output;
+        }//WORKS
+
+
+
+
+
+
+
+
+        //Commented out methods
+        /*public void ImportFocusPixelMap(string filename)
+        {
+
+        }*///TO DO TO DO
+
+        /*public RawDataTool()
+        {
+            focusPixelPositions = new ArrayList();
+        }*///TO DO TO DO
+
+        /*void SwapColumn(int column1, int column2)
+        {
+            int[] temp = new int[rawHeight];
+            for (int i = 0; i < rawHeight; i++)
+            {
+                temp[i] = rawData[column1, i];
+                rawData[column1, i] = rawData[column2, i];
+                rawData[column2, i] = temp[i];
+            }
+        }*///NEEDS TEST, BUT NOT IN INTEREST
+
+        /*void SwapRow(int row1, int row2)
+        {
+            int[] temp = new int[rawWidth];
+            for (int i = 0; i < rawWidth; i++)
+            {
+                temp[i] = rawData[i, row1];
+                rawData[i, row1] = rawData[i, row2];
+                rawData[i, row2] = temp[i];
+            }
+        }*///NEEDS TEST, BUT NOT IN INTEREST
+
+        /*public void ImportRawDataArray(string filename)
+        {
+            string inputdata = File.ReadAllText(@filename);
+        }*///TO DO TO DO
+    }
+}
